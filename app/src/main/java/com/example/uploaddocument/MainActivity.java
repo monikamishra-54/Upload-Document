739fb47a458d4d69adf1619900bf6d429c2b450e
lytechx <lytechxdigital@gmail.com>
2020-04-24 17:57:00 +0530

    bug fix

---
+++ app/src/main/java/com/example/uploaddocument/MainActivity.java
@@ -0,0 +1,270 @@
+package com.example.uploaddocument;
+
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.StringRequestListener;
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int FILE_PICKER_REQUEST_CODE = 1;
+    private String docPath;
+    private static final String ROOT_URL = "https://royalreview.website/doctor/wp-json/api/v1/add_doctor_alldocuments";
+    File file;
+    private String docFileName;
+    TextView txt_path,txt_filename,upload_doc;
+    LinearLayout lin_doc;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        txt_path=findViewById(R.id.txt_path);
+        txt_filename=findViewById(R.id.txt_filename);
+        upload_doc=findViewById(R.id.upload_doc);
+        lin_doc=findViewById(R.id.lin_doc);
+
+        findViewById(R.id.txt_select_doc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!checkPermission()) {
+
+                    requestPermission();
+
+                } else {
+
+                  Toast.makeText(MainActivity.this,"Permission already granted.",Toast.LENGTH_LONG).show();
+
+                    launchChooser();
+                }
+
+
+
+            }
+        });
+
+
+        upload_doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadDocument();
+            }
+        });
+
+
+    }
+
+    private void uploadDocument() {
+
+
+        if (docPath == null) {
+            Toast.makeText(this, "please select Document ", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+
+            file = new File(docPath);
+
+            Log.d("onResponse", docPath);
+
+            String url = ROOT_URL;
+
+            AndroidNetworking.upload(url)
+                    .addMultipartFile("doc_file", file)
+                    .addMultipartParameter("id", "402")
+                    .addMultipartParameter("doc_name", docFileName)
+                    .setPriority(Priority.MEDIUM)
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsString(new StringRequestListener() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Log.e(TAG, "onResponse: " + response);
+                            Log.d("onResponse: ", response);
+                            Toast.makeText(MainActivity.this, response + "", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        @Override
+                        public void onError(ANError error) {
+
+
+                        }
+                    });
+
+
+        }
+
+
+
+
+
+
+    }
+
+    private void launchChooser() {
+
+        new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(FILE_PICKER_REQUEST_CODE)
+                .withHiddenFiles(false)
+                .withFilter(Pattern.compile(".*/*")) // Filtering files and directories by file name using regexp
+                .withTitle("Select file")
+                .withFilterDirectories(true)
+                .start();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            String path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            File file = new File(path);
+            displayFromFile(file);
+            if (path != null) {
+                Log.d("Path: ", path);
+                docPath = path;
+                Toast.makeText(this, "Picked file: " + path, Toast.LENGTH_LONG).show();
+                txt_path.setText(docPath);
+                txt_filename.setText(docFileName);
+                lin_doc.setVisibility(View.VISIBLE);
+            }
+        }
+
+    }
+
+
+    private void displayFromFile(File file) {
+
+        Uri uri = Uri.fromFile(new File(file.getAbsolutePath()));
+        docFileName = getFileName(uri);
+
+
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (locationAccepted && cameraAccepted){
+
+                    }
+
+                    else {
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},
+                                                            PERMISSION_REQUEST_CODE);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+                }
+
+
+                break;
+        }
+
+
+    }
+
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},
+                PERMISSION_REQUEST_CODE);
+
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+
+}
